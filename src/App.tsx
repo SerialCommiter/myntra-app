fd87f4d9337a30114c9fc99009e51a229659e557
sprtkd <supdevan@gmail.com>
2021-08-13 13:50:50 +0530

    lint rm

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProductPage from "./components/page/ProductPage/ProductPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Card
+        brand="Daniel"
+        imgSrc="http://assets.myntassets.com/assets/images/2466435/2018/5/21/265434b9-de22-4b2c-9ca2-ded4c01ef5801526878966488-Daniel-Klein-Women-Black-Analogue-Watch-DK11421-5-2221526878966271-1.jpg"
+        name="HIGHLANDER Men Black Solid Denim Jacket"
+        price={959}
+        discountLabel="(70% OFF)"
+        mrp={3750}
+      /> */}
+
+      <ProductPage />
     </div>
   );
 }
